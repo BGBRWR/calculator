010791f88d5a8d86eb0bda59c0df76f3698fc97b
Austin Walker <bgbrwr@gmail.com>
2016-11-15 22:23:26 -0700

    removed some spacing and console.logs

--- calculator.js
+++ calculator.js
@@ -9,13 +9,9 @@ function numberWithCommas(x) {
     return parts.join(".");
 }
 
-
-
-
 // Add onclick event to all the keys and perform operations
 for(var i = 0; i < keys.length; i++) {
 	keys[i].onclick = function(e) {
-
 
 		// Get the input and button values
 		var input = document.querySelector('.screen');
@@ -75,8 +71,7 @@ for(var i = 0; i < keys.length; i++) {
 				// Here, '.' matches any character while $ denotes the end of string, so anything (will be an operator in this case) at the end of string will get replaced by new operator
 				input.innerHTML = inputVal.replace(/.$/, btnVal);
 			}
-
-			decimalAdded =false;
+			decimalAdded = false;
 		}
 
 		// Now only the decimal problem is left. We can solve it easily using a flag 'decimalAdded' which we'll set once the decimal is added and prevent more decimals to be added once it's set. It will be reset when an operator, eval or clear key is pressed.
